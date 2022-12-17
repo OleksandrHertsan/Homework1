5e1aefadf607300bf906d07955d45ccd88667fc3
Alex <alexgercan88@gmail.com>
2022-12-17 17:38:35 +0200

    Initial commit

---
+++ MainForPerson.java
@@ -0,0 +1,15 @@
+public class MainForPerson {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Alex";
+        person.age = 19;
+        person.height = "2 meters";
+        person.weight = 100;
+
+        Person secondPerson = new Person();
+        person.info();
+
+    }
+
+}
